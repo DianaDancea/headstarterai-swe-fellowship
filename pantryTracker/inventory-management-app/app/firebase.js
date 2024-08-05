27b8d36ed83a36540b4da168948e79cd5407923a
DianaDancea <82067628+DianaDancea@users.noreply.github.com>
2024-08-05 00:25:06 -0400

    Update firebase.js

--- pantryTracker/inventory-management-app/app/firebase.js
+++ pantryTracker/inventory-management-app/app/firebase.js
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCap8z4uv_z06y_6V1Py4-NAgibTQYyKEg",
+  apiKey: "API_KEY",
   authDomain: "inventory-management-app-hs.firebaseapp.com",
   projectId: "inventory-management-app-hs",
   storageBucket: "inventory-management-app-hs.appspot.com",
-  messagingSenderId: "370347772035",
-  appId: "1:370347772035:web:cb09816a1d0602e0d933b6",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
   measurementId: "G-ZJXVY767MK",
   storageBucket: "inventory-management-app-hs.appspot.com"
 };
